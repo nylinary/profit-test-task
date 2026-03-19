cb4de471bb271af668fe833d4a404a48d7cf51d5
Nikita Sysoev <nylinary.work@yandex.com>
2026-03-19 16:47:08 +0300

    main feat: include routers

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from app.api.v1.routers.events import router as events_router
+from app.api.v1.routers.ws import router as ws_router
 from app.core.config import settings
 from app.core.logging import setup_logging
 
@@ -8,6 +10,9 @@ def create_app() -> FastAPI:
     setup_logging()
 
     app = FastAPI(title="Notification Service", version="1.0.0", debug=settings.debug)
+
+    app.include_router(events_router)
+    app.include_router(ws_router)
 
     return app
 
