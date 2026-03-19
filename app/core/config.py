e954caf86cf89f1ce0c792212d6a68f2876b87ab
Nikita Sysoev <nylinary.work@yandex.com>
2026-03-19 12:57:45 +0300

    main feat: add config

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    debug: bool = True
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
+from app.core.config import settings
 from app.core.logging import setup_logging
 
 
 def create_app() -> FastAPI:
     setup_logging()
 
-    app = FastAPI(title="Notification Service", version="1.0.0")
+    app = FastAPI(title="Notification Service", version="1.0.0", debug=settings.debug)
 
     return app
 
