d97a10124485f9c77f3a1777bd2712e839ddc66f
Nikita Sysoev <nylinary.work@yandex.com>
2026-03-19 12:55:32 +0300

    main feat: setup logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from app.core.logging import setup_logging
+
 
 def create_app() -> FastAPI:
+    setup_logging()
+
     app = FastAPI(title="Notification Service", version="1.0.0")
 
     return app
